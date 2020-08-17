01477e48abe61950850e95f6df7e05b11a2f9851
Kathryn DiPippo <dipippo.k@gmail.com>
2020-08-17 18:23:58 -0400

    Reduced palette with threshold 6 displays

--- src/app/palette/palette.component.ts
+++ src/app/palette/palette.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import palettes from '../../assets/palettes.json';
 import Color from 'color';
-// import * as DeltaE from 'delta-e';
+import * as DeltaE from 'delta-e';
 
 @Component({
   selector: 'app-palette',
@@ -17,6 +17,8 @@ export class PaletteComponent implements OnInit {
   palettes: any = palettes;
   palette: any;
   sorted: any;
+  reduced: any;
+  reduced_remainder_count: number;
 
   constructor(
     private route: ActivatedRoute,
@@ -30,7 +32,14 @@ export class PaletteComponent implements OnInit {
         this.error = "error: slug not found";
       } else {
         this.palette = this.palettes[this.slug];
-        this.sorted = this.get_sorted_hex_codes(this.palette.colors[this.im].hex_codes);
+        let color_objects = this.convert_hex_codes_to_colors(this.palette.colors[this.im].hex_codes);
+        this.sorted = this.get_sorted_hex_codes(color_objects);
+
+        this.reduced = this.get_reduced_palette(color_objects, 6);
+        this.reduced_remainder_count = this.palette.columns - (this.reduced.length % this.palette.columns);
+        if (this.reduced_remainder_count == this.palette.columns) {
+          this.reduced_remainder_count = 0;
+        }
       }
     });
   }
@@ -64,14 +73,60 @@ export class PaletteComponent implements OnInit {
     return hex_codes;
   }
 
-  get_sorted_hex_codes(hex_codes) {
-    let colors = this.convert_hex_codes_to_colors(hex_codes);
+  get_sorted_hex_codes(color_objects) {
     return {
-      "original": this.convert_colors_to_hex_codes(Array.prototype.slice.call(colors)),
-      "hue": this.convert_colors_to_hex_codes(Array.prototype.slice.call(colors).sort(this.hue_compare)),
-      "saturation": this.convert_colors_to_hex_codes(Array.prototype.slice.call(colors).sort(this.saturation_compare)),
-      "lightness": this.convert_colors_to_hex_codes(Array.prototype.slice.call(colors).sort(this.lightness_compare)),
+      "original": this.convert_colors_to_hex_codes(Array.prototype.slice.call(color_objects)),
+      "hue": this.convert_colors_to_hex_codes(Array.prototype.slice.call(color_objects).sort(this.hue_compare)),
+      "saturation": this.convert_colors_to_hex_codes(Array.prototype.slice.call(color_objects).sort(this.saturation_compare)),
+      "lightness": this.convert_colors_to_hex_codes(Array.prototype.slice.call(color_objects).sort(this.lightness_compare)),
     };
+  }
+
+  get_score_from_delta(delta, threshold) {
+    // Ranges adjusted based on makeup perception.
+    switch (true) {
+      case (delta <= 1):
+        return 1000;
+      case (delta <= threshold):
+        return 1;
+      default:
+        return 0;
+    }
+  }
+
+  get_reduced_palette(color_objects, threshold) {
+    while (true) {
+      let scores = Array(color_objects.length).fill(0);
+      for (let a = 0; a < color_objects.length; a++) {
+        for (let b = 0; b < color_objects.length; b++) {
+          if (a == b) {
+            continue;
+          }
+          var color1 = {
+            L: color_objects[a].l(),
+            A: color_objects[a].a(),
+            B: color_objects[a].b()
+          };
+          var color2 = {
+            L: color_objects[b].l(),
+            A: color_objects[b].a(),
+            B: color_objects[b].b()
+          };
+
+          // 2000 formula
+          let delta = DeltaE.getDeltaE00(color1, color2);
+          scores[a] += this.get_score_from_delta(delta, threshold);
+        }
+      }
+      let max = Math.max( ...scores );
+      if (max == 0) {
+        return this.convert_colors_to_hex_codes(color_objects);
+      }
+      var maxIndex = scores.reduce(function(highestIndex, element, index, array){
+        return element > array[highestIndex] ? index : highestIndex;
+      }, 0);
+      color_objects.splice(maxIndex, 1);
+    }
   }
 
 }
